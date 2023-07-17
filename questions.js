c7a575dac7fa76df7aeab18c230d627b8301b635
Marcele Eller Gusmao <dev.marcele@gmail.com>
2023-07-17 17:51:29 -0300

    final commit, corrections

---
+++ questions.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        question: "O que Ada Lovelace inventou?",
+        answers: [
+            { option: "Instruções para o primeiro computador", correct: true },
+            { option: "O primeiro computador", correct: false} ,
+            { option: "A lâmpada", correct: false },
+        ],
+    },
+    {
+        question: "Qual era o nome do matemático com quem ela trabalhou em um projeto de computação?",
+        answers: [
+            { option:"Isaac Newton", correct: false },
+            { option:"Charles Babbage", correct: true },
+            { option:"Alan Turing", correct: false },
+        ],
+    },
+    {
+        question: "Com quantos anos Lovelace veio a falecer?",
+        answers: [
+            { option:"28 anos", correct: false },
+            { option:"39 anos", correct: false },
+            { option:"36 anos", correct: true },
+        ],
+    },
+];
